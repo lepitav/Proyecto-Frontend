51ddda10a7719b8137c1b331778fc4bf4f9e9ed2
lepitav <luiseduardopita@hotmail.com>
2018-03-31 15:57:10 -0400

    2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './assets/styles/layout.css'
+import './assets/styles/framework.css'
+import './assets/styles/fontawesome-4.4.0.min.css'
+import './assets/styles/css.css'
+
+//import './assets/scripts/jquery.backtotop.js'
+//import './assets/scripts/jquery.min.js'
+//import './assets/scripts/jquery.mobilemenu.js'
+//import './assets/scripts/jquery.placeholder.min.js'
+
 Vue.config.productionTip = false
 
 new Vue({
